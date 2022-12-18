8a8674322d0521342d19abc0c428ad63d3bea7f7
Vitor <vcsoouza@hotmail.com>
2022-12-18 21:14:28 +0200

    Start FinanceApp

---
+++ FinanceApp.Models/Expense.cs
@@ -0,0 +1,7 @@
+﻿namespace FinanceApp.Models
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ FinanceApp.Models/General.cs
@@ -0,0 +1,14 @@
+﻿using FinanceApp.Models.ENUMS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceApp.Models
+{
+    internal class General:Expense
+    {
+        public ExpenseCategories Categories { get; set; }
+    }
+}
